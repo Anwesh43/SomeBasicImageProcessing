32747a228ca3e930bceb9af77d4d0932380ac40b
Anwesh43 <anweshthecool0@gmail.com>
2016-12-19 14:53:08 +0530

    Adding the library

---
+++ basicimageprocessing/src/main/java/com/anwesome/ui/basicimageprocessing/ImageProcessingUtil.java
@@ -0,0 +1,90 @@
+package com.anwesome.ui.basicimageprocessing;
+
+import android.graphics.*;
+import android.media.Image;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.ImageView;
+
+/**
+ * Created by anweshmishra on 19/12/16.
+ */
+public class ImageProcessingUtil {
+    private static Handler handler = new Handler();
+    private static MainpulationHandler grayScaleManipulator = new MainpulationHandler() {
+        @Override
+        public void manipulateImage(Bitmap bitmap, int x, int y, int pixel) {
+            float gs = 0.16f*getRed(pixel)+0.34f*getGreen(pixel)+0.25f*getBlue(pixel);
+            bitmap.setPixel(x,y,Color.argb(Color.alpha(pixel),(int)gs,(int)gs,(int)gs));
+        }
+    };
+    private static int[][] getPixels(Bitmap bitmap) {
+        int pixels[][] = new int[bitmap.getWidth()][bitmap.getHeight()];
+        for(int i=0;i<bitmap.getWidth();i++) {
+            for(int j=0;j<bitmap.getHeight();j++) {
+                pixels[i][j] = bitmap.getPixel(i,j);
+            }
+        }
+        return pixels;
+    }
+    private static int getRed(int pixel) {
+        return Color.red(pixel);
+    }
+    private static int getGreen(int pixel) {
+        return Color.green(pixel);
+    }
+    private static int getBlue(int pixel) {
+        return Color.blue(pixel);
+    }
+    public static void createGrayScaleImage(Bitmap bitmap,ImageView imageView){
+        Bitmap newBitmap = Bitmap.createBitmap(bitmap.getWidth(),bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        int pixels[][] = getPixels(bitmap);
+        int w= bitmap.getWidth(),h = bitmap.getHeight();
+        ImageMainpThread imageMainpThread = new ImageMainpThread(newBitmap,pixels,0,0,w,h,grayScaleManipulator,imageView);
+        new Thread(imageMainpThread).start();
+
+    }
+    private static class ImageMainpThread implements Runnable{
+        private int startI,startJ,endI,endJ;
+        private Bitmap bitmap;
+        private MainpulationHandler mainpulationHandler;
+        private ImageView imageView;
+        private int pixels[][];
+        public ImageMainpThread(Bitmap bitmap, int pixels[][], int startI, int startJ, int endI, int endJ, MainpulationHandler mainpulationHandler, ImageView imageView) {
+            this.startI = startI;
+            this.startJ = startJ;
+            this.endI = endI;
+            this.endJ = endJ;
+            this.bitmap = bitmap;
+            this.pixels = pixels;
+            this.mainpulationHandler = mainpulationHandler;
+            this.imageView = imageView;
+        }
+        public void run() {
+            for(int i=startI;i<endI;i++) {
+                for(int j=startJ;j<endJ;j++) {
+                    mainpulationHandler.manipulateImage(bitmap,i,j,pixels[i][j]);
+                }
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        imageView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                handler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                       imageView.setImageBitmap(bitmap);
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+            }
+        }
+    }
+    private static interface MainpulationHandler {
+         void manipulateImage(Bitmap bitmap,int x,int y,int pixel);
+    }
+}
