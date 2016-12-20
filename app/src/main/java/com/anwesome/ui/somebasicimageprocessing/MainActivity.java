e2f7bf1677b85623b50a0756bfb5036615c5a550
Anwesh43 <anweshthecool0@gmail.com>
2016-12-20 15:01:28 +0530

    Adding code for demo to test the filter to subtract red channel

--- app/src/main/java/com/anwesome/ui/somebasicimageprocessing/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/somebasicimageprocessing/MainActivity.java
@@ -13,12 +13,12 @@ import android.widget.ImageView;
 import com.anwesome.ui.basicimageprocessing.ImageProcessingUtil;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageView imageView = (ImageView)findViewById(R.id.imv);
+        ImageView imageView1 = (ImageView)findViewById(R.id.imv1);
         Drawable drawable = imageView.getDrawable();
         Bitmap carBitmap = BitmapFactory.decodeResource(getResources(),R.drawable.car4);
         Bitmap bitmap = Bitmap.createBitmap(3*carBitmap.getWidth()/10,3*carBitmap.getHeight()/10,Bitmap.Config.ARGB_8888);
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         canvas.drawBitmap(carBitmap,-carBitmap.getWidth()/2,-carBitmap.getHeight()/2,paint);
         canvas.restore();
         ImageProcessingUtil.createGrayScaleImage(bitmap,imageView);
+        ImageProcessingUtil.subtractRedChannelFromBitmap(bitmap,imageView1);
         //imageView.setImageBitmap(bitmap);
         //imageView.setImageBitmap(ImageProcessingUtil.createGrayScaleImage(bitmap));
     }
